63c2eda9c66c4b9cad57e5acf45d6a8b967c216c
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-02-27 19:09:55 +0600

    pppp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,25 @@
+import { SlActionRedo } from "react-icons/sl";
+
 export const Home =() =>{
 
     return(
-        <h1>Home Page</h1>
+        <main className="hero-section main">
+            <div className="container grid grid-two-cols">
+                <div className="hero-content">
+                    <h1 className="heading-xl"> Explore the World, One Country at a Time. </h1>
+                    <p className="paragraph">
+                        Discover the history, culture and beauty of every nation. Sort, search and filter through countries to find the details you need.
+                    </p>
+                    <button className="btn btn-darken btn-inline bg-white-box"> Start Exploring <SlActionRedo /></button>
+                </div>
+
+                <div className="hero-image">
+<img className="banner-image" src="/public/images/world.png" alt="wolrd Image">
+</img>
+</div>
+
+
+            </div>
+        </main>
     )
 }

58b81ade2bbd5c4a9a6d3a2ec1e14a7319e25220
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-02-27 21:06:50 +0600

    ppp

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,29 @@
 export const Contact =() =>{
-
+    const handleFormSubmit = (formData) => {
+        
+        const formInputData = Object.fromEntries(formData.entries());
+        console.log(formInputData);
+      };
     return(
-        <h1>Contact Page</h1>
+        
+        <section className="section-contact">
+            <h2 className="container-title">Contact Us</h2>
+            <div className="contact-wrapper container">
+            <form action={handleFormSubmit}>
+                <input className="form-control" type="text" required autoComplete="false" placeholder="Enter Your Name" name="username">
+                </input>
+                <input type="email" className="form-control" required autoComplete="false" placeholder="Enter Your email" name="email">
+                </input>
+                <textarea className="form-control" rows="10" placeholder="Enter Your message" name="message" required autoComplete="false">
+
+                </textarea>
+
+<button type="submit" value="send">Send</button>
+            </form>
+            </div>
+            
+
+            </section>
+
     )
 }

0a8cd3c6c1ae10d61a8ab084668c246e50e5a351
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-02-27 21:34:52 +0600

    hhhhh

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,9 +1,49 @@
-export const Footer = () =>{
+import footerContact from "/src/API/FooterAPI";
+import { IoCallSharp } from "react-icons/io5";
+import { MdPlace } from "react-icons/md";
+import { TbMailPlus } from "react-icons/tb";
 
+import { NavLink } from "react-router-dom";
 
-    return(
-        <>
-        <h2>Footer Page</h2>
-        </>
-    )
-}
+export const Footer = () => {
+  const footerIcon = {
+    MdPlace: <MdPlace />,
+    IoCallSharp: <IoCallSharp />,
+    TbMailPlus: <TbMailPlus />,
+  };
+
+  return (
+    <footer className="footer-section">
+      <div className="container grid grid-three-cols">
+        {footerContact.map((curData, index) => {
+          const { icon, title, details } = curData;
+          return (
+            <div className="footer-contact" key={index}>
+              <div className="icon">{footerIcon[icon]}</div>
+              <div className="footer-contact-text">
+                <p>{title}</p>
+                <p>{details}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="copyright-area">
+        <div className="container">
+          <div className="grid grid-two-cols">
+            <div className="copyright-text">
+              <p>
+                Copyright &copy; 2024, All Right Reserved
+                <NavLink to="https://abdullah-hasan467.github.io/-My-portfolio/" target="_blank">
+                  Md. Abdullah Al Hasan
+                </NavLink>
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
